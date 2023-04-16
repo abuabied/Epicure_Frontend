4ca8a6a96bd85a014ed242a5249091147d4e30d6
Mohamad AbuAbied <82101799+abuabied@users.noreply.github.com>
2023-04-16 22:39:11 +0300

    update fetch link

--- src/constants/fetchesAPIs.ts
+++ src/constants/fetchesAPIs.ts
@@ -1,20 +1,15 @@
-export const allAPI = "http://localhost:3001/api/restaurants/getRestaurants";
-export const popularAPI =
-  "http://localhost:3001/api/restaurants/getPopularRestaurants";
-export const newAPI =
-  "http://localhost:3001/api/restaurants/getNewRestaurants";
-export const openAPI =
-  "http://localhost:3001/api/restaurants/getOpenRestaurants";
+const triggerURL = "https://us-central1-epicure-server-1.cloudfunctions.net/app";
+export const allAPI = `${triggerURL}/api/restautrants/getRestaurants`;
+export const popularAPI = `${triggerURL}/api/restautrants/getPopularRestaurants`;
+export const newAPI = `${triggerURL}/api/restautrants/getNewRestaurants`;
+export const openAPI = `${triggerURL}/api/restautrants/getOpenRestaurants`;
 export const byNameAPI = (restaurantName: String) => {
-  return `http://localhost:3001/api/restaurants/getRestaurantByName?name=${restaurantName}`;
+  return `${triggerURL}/api/restautrants/getRestaurantByName?name=${restaurantName}`;
 };
+export const getChefOfTheWeekAPI = `${triggerURL}/api/chefs/getChefofTheWeek`;
+export const getAllChefsAPI = `${triggerURL}/api/chefs/getAllChefs`;
+export const getPopularChefsAPI = `${triggerURL}/api/chefs/getPopularChefs`;
+export const getNewChefsAPI = `${triggerURL}/api/chefs/getNewChefs`;
 
-export const getChefOfTheWeekAPI =
-  "http://localhost:3001/api/chefs/getChefofTheWeek";
-export const getAllChefsAPI = "http://localhost:3001/api/chefs/getAllChefs";
-export const getPopularChefsAPI =
-  "http://localhost:3001/api/chefs/getPopularChefs";
-export const getNewChefsAPI = "http://localhost:3001/api/chefs/getNewChefs";
-
-export const registerAPI = "http://localhost:3001/api/auth/register";
-export const loginAPI = "http://localhost:3001/api/auth/login";
+export const registerAPI = `${triggerURL}/api/auth/register`;
+export const loginAPI = `${triggerURL}/api/auth/login`;
